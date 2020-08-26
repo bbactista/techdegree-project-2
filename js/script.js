63f5284be62ea49a3abf125f9d816815e86a9120
bbactista <69237600+bbactista@users.noreply.github.com>
2020-08-27 00:12:55 +0400

    Meets Expectation

--- js/script.js
+++ js/script.js
@@ -3,41 +3,15 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
+// Global Variables
 const listItems = document.getElementsByClassName('student-item cf');
 const itemsPerPage = 10;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
+// showPage function: Shows 10 students per page
 function showPage (list, page) {
-   let startIndex = (page * itemsPerPage) - itemsPerPage;
-   let endIndex = page * itemsPerPage;
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
    for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex){
          list[i].style.display = 'block';
@@ -47,13 +21,7 @@ function showPage (list, page) {
    }
 }
 
-showPage(listItems, 1);
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
+// appendPageLinks function: Appends students and links to the page
 const appendPageLinks = (list) => {
    const totalPages = Math.ceil (listItems.length / itemsPerPage);
    const page = document.getElementsByClassName('page')[0];
@@ -68,17 +36,18 @@ const appendPageLinks = (list) => {
       const a = document.createElement('a');
       li.appendChild(a);
       a.textContent = `${i + 1}`;
-      a.addEventListener ('click', (e) => {
+      a.addEventListener ('click', (e) => { // When links are clicked, the corresponding page of 10 students will show.
+         const buttons = document.querySelectorAll('.pagination a');
          showPage(listItems, i + 1)
-         for (let j = 0; j < a.length; j++) {
-            button.classList.remove('active');
+         for (let j = 0; j < buttons.length; j++) { // Allow only the clicked link to have the active class.
+            buttons[j].classList.remove('active');
          }
          e.target.classList.add('active');
       });
    }
 }
+showPage(listItems, 1)
 appendPageLinks(listItems);
 
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
